a1b7664668d43f04cb5a79031942cf7ef129a9b5
CODIFY <frankmwangi8118@gmail.com>
2025-03-25 13:19:15 +0300

    fix: fixed the eureka-service-url

--- school-service/src/main/java/com/codify/school_service/SchoolServiceApplication.java
+++ school-service/src/main/java/com/codify/school_service/SchoolServiceApplication.java
@@ -2,9 +2,9 @@ package com.codify.school_service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
-
 @SpringBootApplication
 public class SchoolServiceApplication {
 
